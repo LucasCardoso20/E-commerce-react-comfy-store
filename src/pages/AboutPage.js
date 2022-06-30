c47851b4ecaef20bd23971464e6ce90dbb27d68a
LucasCardoso20 <lucascardosodeoliveira63@gmail.com>
2022-06-30 19:06:14 -0300

    first commit

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import styled from 'styled-components'
+import { PageHero } from '../components'
+import aboutImg from '../assets/hero-bcg.jpeg'
+
+const AboutPage = () => {
+  return <main>
+    <PageHero title="about"/>
+    <Wrapper className='page section section-center'>
+      <img src={aboutImg} alt='nice desk'/>
+      <article>
+        <div className='title'>
+          <h2>our story</h2>
+          <div className='underline'></div>
+        </div>
+        <p>
+        Jelly sweet roll jelly beans biscuit pie macaroon chocolate donut. Carrot cake caramels pie sweet apple pie tiramisu carrot cake. Marzipan marshmallow croissant tootsie roll lollipop. 
+        Cupcake lemon drops bear claw gummies. Jelly bear claw gummi bears lollipop cotton candy gummi bears chocolate bar cake cookie. Cupcake muffin danish muffin cookie gummies.
+        </p>
+      </article>
+    </Wrapper>
+  </main>
+}
+
+const Wrapper = styled.section`
+  display: grid;
+  gap: 4rem;
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    height: 500px;
+    object-fit: cover;
+  }
+  p {
+    line-height: 2;
+    max-width: 45em;
+    margin: 0 auto;
+    margin-top: 2rem;
+    color: var(--clr-grey-5);
+  }
+  .title {
+    text-align: left;
+  }
+  .underline {
+    margin-left: 0;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`
+export default AboutPage
